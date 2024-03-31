0021caa2ad9a8ef418fc525c35df19cc3d2ef324
Enesozd1 <94735890+Enesozd1@users.noreply.github.com>
2024-03-31 19:48:34 +0600

    cor error

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
-const cors = require("cors");
 const corsOptions = {
     origin: "https://eucway.com",
+    methods: ['GET', 'POST'],
   };
   
 const Stripe = require("stripe");
