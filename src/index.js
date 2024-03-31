be0544aa4bd349b0d29d1693f590e171e90b9714
Enesozd1 <94735890+Enesozd1@users.noreply.github.com>
2024-03-31 18:27:59 +0600

    changes

--- src/index.js
+++ src/index.js
@@ -9,17 +9,20 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const corsOptions = {
+    origin: "https://eucway.com",
+  };
+  
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_KEY)
 const bodyParser = require('body-parser');
 const Schema = mongoose.Schema
 //const sendinblue = require('./api/sendinblue');
 const dotenv = require("dotenv")
-
 dotenv.config()
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 
 //Database connection
