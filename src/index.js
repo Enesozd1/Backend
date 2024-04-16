d62448d6d455a420a75178015f98369299007143
Enesozd1 <94735890+Enesozd1@users.noreply.github.com>
2024-04-16 12:35:42 +0600

    product availability

--- src/index.js
+++ src/index.js
@@ -397,7 +397,7 @@ app.post('/addtocart',fetchUser, async (req,res) =>{
     let userData = await Users.findOne({_id:req.user.id});
     userData.cartData[req.body.itemId] += 1;
     await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
-    res.send("Added")
+    res.send(userData.available)
 })
 
 //endpoint for remove cartdata
